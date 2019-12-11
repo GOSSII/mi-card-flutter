7a36c8f1c9bd004a0636947e483b9222e4a3cc78
Dipesh Goswami <dipeshpuri9@gmail.com>
2019-12-11 11:43:35 -0500

    Single child widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,16 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            child: Text('Hello World'),
+            color: Colors.white,
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.all(20.0),
+            padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 20.0),
+          ),
+        ),
       ),
     ),
   );
