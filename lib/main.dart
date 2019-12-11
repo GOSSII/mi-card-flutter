adc242e452d2eac7093749e0b0f5df98104f7222
Dipesh Goswami <dipeshpuri9@gmail.com>
2019-12-11 11:57:59 -0500

    Multi child widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,36 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            child: Text('Hello World'),
-            color: Colors.white,
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.all(20.0),
-            padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 20.0),
-          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                child: Center(child: Text('Container 1')),
+                color: Colors.white,
+                height: 100.0,
+                width: 100.0,
+              ),
+              Container(
+                child: Center(child: Text('Container 2')),
+                color: Colors.blue,
+                height: 100.0,
+                width: 100.0,
+              ),
+              Container(
+                child: Center(child: Text('Container 3')),
+                color: Colors.redAccent,
+                height: 100.0,
+                width: 100.0,
+              ),
+            ],
+          )
         ),
       ),
     ),
   );
 }
+
+
+
+//margin: EdgeInsets.all(20.0),
+//padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 20.0),
